1ddab75bad6262b99395ddabd22294adb3700336
Narnillian <ner.baby@gmail.com>
2022-06-16 12:55:01 -0700

    Slight modification because I tried to do something
    Will I rewrite again? Will I not rewrite again?
    It would be Python. That's all I know

--- kompile.c
+++ kompile.c
@@ -11,20 +11,23 @@ int main(int argc, char** argv) {
         return 0;
     } else if (argc == 2 && !strcmp(argv[1], "-h")) {
         printf("Custom command to compile and execute Kotlin with one command, because the actual command is too long:\n");
-        printf("Original command:   kotlinc source.kt -include-runtime -d result.jar [-verbose]\n");
+        printf("Original command:   kotlinc source.kt -include-runtime -d result.jar [options]\n");
         printf("Custom command usage:\n");
         printf("\tcompkot source.kt result.jar [options]\n");
         printf("\tkompile source.kt result.jar [options]\n");
         printf("[options] (such as `-verbose`) applies to both Kotlin compilation *and* Java .jar execution\n");
         printf("\nMy puns are superb\t\t\t\tThis is a custom command, not APT, but my APT has Super Cow Powers\t(apt-get moo)\n");
     } else {
-        printf("\nCompiling %s to %s\n",argv[1],argv[2]);
-        printf("------------------\n");
         {
-            char *str1 = strcat(strcat(strcpy(malloc(strlen("kotlinc ") + strlen(argv[1]) + strlen(" -include-runtime -d") + 1), "kotlinc "), argv[1]),  " -include-runtime -d ");
+            char *str1 = strcat(strcat(strcpy(malloc(strlen("kotlinc ") + strlen(argv[1]) + strlen(" -include-runtime -d") + 1), "kotlinc "), argv[1]),  " -include-runtime -d");
             char *str2 = malloc(1);
             *str2 = 0;
+            char ignore = 0;
             for (int i = 2; i < argc; i++) {
+                if (*argv[i] == ']') {ignore = 0;continue;}
+                if (ignore) continue;
+                if (*argv[i] == '[') {ignore = 1;continue;}
+
                 str2 = realloc(str2, strlen(str2) + strlen(argv[i]) + 2);
                 strcat(str2, " ");
                 strcat(str2, argv[i]);
@@ -32,6 +35,8 @@ int main(int argc, char** argv) {
             str1 = strcpy(malloc(strlen(str1) + strlen(str2) + 2), str1);
             strcat(str1,str2);
             //printf("COMMAND:   \"%s\"\n",str1);
+            printf("\nCompiling \"%s\" to \"\033[1;91m%s\033[0m\" via \"\033[1;36m%s\033[0m\"\n",argv[1],argv[2],str1);
+            printf("------------------\n");
             result = system(str1);
             free(str1);
             free(str2);
@@ -39,6 +44,7 @@ int main(int argc, char** argv) {
         if (result) {
             printf("------------------\n");
             printf("Compilation failed\n");
+            return result;
         } else {
             printf("Compilation completed\n");
             printf("------------------\n");
@@ -60,8 +66,8 @@ int main(int argc, char** argv) {
                 printf("------------------\n");
                 system(str1);
                 free(str1);
+                return 0;
             }
         }
     }
-    return 0;
 }
